3a1601f4160c764d6d736c71bb13f49235dc0010
Harikapatha <42451263+Harikapatha@users.noreply.github.com>
2024-03-31 22:25:56 -0500

    note-taker

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Note-Taker
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+This is a simple note-taking application that allowd users to create, save, view, and delete notes. Users can also send a GET HTTP request to view the all of the notes objects with a unique note id.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contribute](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run following command:
+npm i
+Application is deployed on render.com.
+
+## Usage
+Click "Get Started" to begin.
+Click on "Note Title" to enter a Title of a new note, then click "Note Text" to enter the note.
+Click "Save Note" to add your note.
+Notes will appear in a list on the left side of the screen.
+Click a particular note to see the note details.
+Click the Trash Can icon to delete a note.
+
+## Credits
+N/A
+
+## License
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Contributing
+Fork the github repo, make your changes, send me a pull request to review changes.
+
+## Tests
+N/A
+
+## Questions
+To contact me with any questions regarding this project:
+Email me at: <harikanap518@gmail.com>
---
+++ routes/notes.js
@@ -0,0 +1,60 @@
+//import express and Router method 
+const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+//GET route for retrieving db.json data
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+//POST route for creating a new note
+notes.post('/', (req,res) => {
+    //Deconstructing assignment for the items in req.body
+    const { title, text } = req.body;
+    //if all of the required properties are in the req.body, create a new note
+    if ( title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        //create a new response object with a status 
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        //send it as a response
+        res.json(response);
+    } else {
+        res.json('Error in posing a new note');
+    }
+});
+
+//Delete route for deleting a targetted item in db.json
+notes.delete('/:id', (req,res) => {
+    //get the id to delete from the params in the request 
+    const idToDelete = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((notes) => {
+        //loop through all of the items in the db.json to check if its id matches the id to delete.
+        //then, update the notes without the note with a matching id
+        const updatedNotes = notes.filter((note) => note.id !== idToDelete);
+        //update the db.json with the updated notes
+        writeToFile('./db/db.json', updatedNotes);
+
+        const response = {
+            status: 'success',
+            body: updatedNotes,
+        };
+        res.json(response);
+    })
+    .catch((error) => {
+        console.error(error);
+        res.json("Error deleting the note");
+    });
+});
+//exporting notes router.
+module.exports = notes;
